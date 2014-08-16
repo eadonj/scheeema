b5d92167c16b1b47d0f3348dd9c53c7a7bc23774
John Gesimondo <john@jmondo.com>
2014-08-15 17:22:35 -0700

    load up associations
    use models instead of tables

--- app/models/scheeema/table.rb
+++ app/models/scheeema/table.rb
@@ -1,29 +1,24 @@
 module Scheeema
   class Table
-    attr_reader :name
+    attr_reader :name, :model
     private_class_method :new
 
     def self.all
-      names = connection.tables - ['schema_migrations']
-      names.map { |table_name| new(table_name) }
+      Dir[Rails.root.join('app', 'models', '*.rb')].each { |file| require file }
+      ActiveRecord::Base.descendants.map { |model| new(model) }
     end
 
-    def initialize(name)
-      @name = name
+    def initialize(model)
+      @model = model
+      @name = model.table_name
     end
 
     def columns
-      connection.columns(name).map { |ar_column| Column.new(ar_column) }
+      model.columns.map { |ar_column| Column.new(ar_column) }
     end
 
-    private
-
-    def connection
-      ActiveRecord::Base.connection
-    end
-
-    def self.connection
-      ActiveRecord::Base.connection
+    def associations
+      model.reflect_on_all_associations.map { |ar_association| Association.new(ar_association) }
     end
   end
 
@@ -35,5 +30,35 @@ module Scheeema
     end
 
     def_delegators :@ar_column, :name, :type
+  end
+
+  class Association
+    def initialize(ar_association)
+      @ar_association = ar_association
+    end
+
+    delegate :name, :foreign_key, to: :ar_association
+
+    def type
+      ar_association.macro
+    end
+
+    def foreign_table
+      # if there's a table that /looks/ like a join table (has the right name) ActiveRecord will return it here,
+      # regardless of whether that association uses it. Only do this where we know we need to.
+      if type == :has_and_belongs_to_many
+        ar_association.join_table
+      else
+        ar_association.table_name
+      end
+    end
+
+    def primary_key
+      ar_association.active_record_primary_key
+    end
+
+    private
+
+    attr_reader :ar_association
   end
 end
--- spec/models/scheeema/table_spec.rb
+++ spec/models/scheeema/table_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Scheeema::Table do
   let(:philosophers_table) { Scheeema::Table.all.detect { |table| table.name == 'philosophers' } }
+
   describe '.all' do
     it 'returns an array' do
       expect(Scheeema::Table.all).to be_a(Array)
@@ -29,7 +30,7 @@ describe Scheeema::Table do
       expect(philosophers_table.columns).to be_a(Array)
     end
 
-    it 'includes the first column on philosophers' do
+    it 'includes the last name column' do
       expect(last_name_column).to be_present
     end
 
@@ -39,6 +40,68 @@ describe Scheeema::Table do
 
     it 'includes the column type' do
       expect(last_name_column.type).to eq(:string)
+    end
+  end
+
+  describe '#associations' do
+    it 'returns an array' do
+      expect(philosophers_table.associations).to be_a(Array)
+    end
+
+    context 'has_and_belongs_to_many' do
+      let(:paradigms_association) { philosophers_table.associations.detect { |assoc| assoc.name == :paradigms } }
+
+      it 'includes the paradigms association' do
+        expect(paradigms_association).to be_present
+      end
+
+      it 'includes the association type' do
+        expect(paradigms_association.type).to eq(:has_and_belongs_to_many)
+      end
+
+      it 'includes the name' do
+        expect(paradigms_association.name).to eq(:paradigms)
+      end
+
+      it 'includes the foreign table' do
+        expect(paradigms_association.foreign_table).to eq('paradigms_philosophers')
+      end
+
+      it 'includes the foreign_key' do
+        expect(paradigms_association.foreign_key).to eq('philosopher_id')
+      end
+
+      it 'includes the primary_key' do
+        expect(paradigms_association.primary_key).to eq('id')
+      end
+    end
+
+    context 'has_many' do
+      let(:exemplar_association) { philosophers_table.associations.detect { |assoc| assoc.name == :exemplar_paradigms } }
+
+      it 'includes the paradigms association' do
+        expect(exemplar_association).to be_present
+      end
+
+      it 'includes the association type' do
+        expect(exemplar_association.type).to eq(:has_many)
+      end
+
+      it 'includes the name' do
+        expect(exemplar_association.name).to eq(:exemplar_paradigms)
+      end
+
+      it 'includes the foreign table' do
+        expect(exemplar_association.foreign_table).to eq('paradigms')
+      end
+
+      it 'includes the foreign_key' do
+        expect(exemplar_association.foreign_key).to eq('exemplar_id')
+      end
+
+      it 'includes the primary_key' do
+        expect(exemplar_association.primary_key).to eq('id')
+      end
     end
   end
 end
