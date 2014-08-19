74e7e8114b30b9e4baf6fbeb49095c1645cc19a8
Eadon <eadonj@gmail.com>
2014-08-18 22:57:39 -0700

    added tests to philosopher model

--- spec/models/philosopher_spec.rb
+++ spec/models/philosopher_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Philosopher do
   it { should have_and_belong_to_many :paradigms }
   it { should have_many :exemplar_paradigms }
+  it { should have_one :student }
+  it { should belong_to :mentor }
+  it { should have_one :feathered_quill }
+  it { should have_one :esoteric_tool_set }
 end
